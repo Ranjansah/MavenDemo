a4742a5bcf93e8726c23632967e236e54d865159
Ranjansah <91522154+Ranjansah@users.noreply.github.com>
2022-08-04 21:57:38 +0530

    Update demo.java

--- src/main/java/MSA/MavenDemo/demo.java
+++ src/main/java/MSA/MavenDemo/demo.java
@@ -6,6 +6,7 @@ public class demo {
 		// TODO Auto-generated method stub
 		System.out.println("Jay Bholenath");
 		System.out.println("Jay Bholenath,Changed by Ranjan");
+		System.out.println("Jay Bholenath");
 	}
 
 }
