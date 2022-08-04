e181b24a3ab71d03627f7d934c26aa8340863b5a
Ranjan <sahaadi99@gmail.com>
2022-08-04 21:45:16 +0530

    First commit

---
+++ src/main/java/MSA/MavenDemo/App.java
@@ -0,0 +1,13 @@
+package MSA.MavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
